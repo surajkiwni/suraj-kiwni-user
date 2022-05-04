73137cc7b07a668656f76e92e9760edd02e209e0
surajkiwni <suraj.phadtare@kiwni.com>
2022-05-04 15:49:15 +0530

    adding profile image edit functionality and chanhe image view to circle image view and adding alert dialog for profile edit by Suraj

--- app/src/main/java/com/kiwni/app/user/fragments/ProfileFragment.java
+++ app/src/main/java/com/kiwni/app/user/fragments/ProfileFragment.java
@@ -1,7 +1,12 @@
 package com.kiwni.app.user.fragments;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +15,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -26,7 +34,12 @@ import cn.pedant.SweetAlert.SweetAlertDialog;
 public class ProfileFragment extends Fragment
 {
     TextView txtLogout, txtEmailAddress, txtMobileNo, txtName, txtEmgContact;
-    ImageView imageBack;
+    ImageView imageBack,editImage;
+
+    private static final int RESULT_CODE_FOR_GALLERY = 100;
+    private static final int RESULT_CODE_FOR_CAMERA = 123;
+    Uri imageUri;
+    private Bitmap bitmap;
 
     String TAG = this.getClass().getSimpleName();
     String userName = "", mobileNumber = "", emailAddress = "", emergencyContact = "";
@@ -54,6 +67,7 @@ public class ProfileFragment extends Fragment
         txtMobileNo = view.findViewById(R.id.txtMobileNo);
         txtName = view.findViewById(R.id.txtName);
         txtEmgContact = view.findViewById(R.id.txtEmgContact);
+        editImage = view.findViewById(R.id.imgProfile);
 
         ((MainActivity) getActivity()).getSupportActionBar().hide();
 
@@ -84,6 +98,52 @@ public class ProfileFragment extends Fragment
             public void onClick(View view)
             {
                 Navigation.findNavController(requireView()).navigate(R.id.action_profileFragment_to_mainActivity);
+            }
+        });
+
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.camera_profile_dialog, null);
+                dialog.setView(dialogView);
+                dialog.setCancelable(false);
+                AlertDialog profileShow = dialog.create();
+
+
+                /*Dialog dialog = new Dialog(ThirdActivity.this, android.R.style.Theme_Light);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.dialog);*/
+
+                TextView txtCamera = dialogView.findViewById(R.id.txtCamera);
+                TextView txtGallery = dialogView.findViewById(R.id.txtGallery);
+                TextView txtCancel = dialogView.findViewById(R.id.txtCancel);
+
+                txtCamera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        openCamera();
+                        profileShow.dismiss();
+
+                    }
+                });
+
+                txtGallery.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        openGallery();
+                        profileShow.dismiss();
+                    }
+                });
+
+                txtCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        profileShow.dismiss();
+                    }
+                });
+                profileShow.show();
             }
         });
 
@@ -122,5 +182,50 @@ public class ProfileFragment extends Fragment
         });
 
         return view;
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, RESULT_CODE_FOR_GALLERY);
+
+    }
+
+    private void openCamera() {
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED){
+
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, RESULT_CODE_FOR_CAMERA);
+
+        }else{
+
+            Intent camera_intent
+                    = new Intent(MediaStore
+                    .ACTION_IMAGE_CAPTURE);
+            startActivityForResult(camera_intent,RESULT_CODE_FOR_CAMERA);
+        }
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_CODE_FOR_GALLERY){
+            imageUri = data.getData();
+            editImage.setImageURI(imageUri);
+        }
+        if (requestCode == RESULT_CODE_FOR_CAMERA) {
+
+            // BitMap is data structure of image file
+            // which stor the image in memory
+            Bitmap photo = (Bitmap)data.getExtras()
+                    .get("data");
+
+            // Set the image in imageview for display
+            editImage.setImageBitmap(photo);
+        }
+
+
+
+
     }
 }
